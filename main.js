cde6f5ff68bdd804f273f8096784dc99bd51720d
unknown <olawaleprofjobs15@gmail.com>
2024-04-10 06:56:02 -0700

    Timer fully done

---
+++ main.js
@@ -0,0 +1,31 @@
+// var timeDisplay = document.getElementById('disPlay');
+// var getTime = document.getElementById('input');
+// let h = 0;
+// let m = 0;
+// let s = 0;
+
+
+// // function timer(){
+// //     var date = new Date();
+// //     var h = date.getHours();
+// //     var m = date.getMinutes();
+// //     var s = date.getSeconds();
+// //     getTime.value = h + ":" + m + ":" + s;
+// //     setInterval(getTime.value, 10000);
+// // }
+
+
+// function startTime(){
+
+//     // console.log("click");
+//     setInterval(function(){
+//         timeDisplay.innerHTML = h + ":" + m + ":" + s;
+//         if(getTime > 0){
+//             timeDisplay.innerHTML == 0;
+//         } else(getTime == 0){
+
+//             alert("input time")
+//         }
+//     }, Number(getTime.value) * 1000)
+
+// }
